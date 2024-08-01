1e025126339130012252a90ac71a4b136e917754
newthings0 <felixn.1162@gmail.com>
2024-08-01 07:50:26 -0700

    Resized Heading

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -17,8 +17,8 @@ const Header = () => {
   return (
     <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
       <div className="flex items-center w-full md:2/3">
-        <Link href="/" className="font-black text-tertiary-dark">
-          Shell Place Houston
+        <Link href="/" className="font-heading text-tertiary-dark">
+          Shell Place Houston/Accommodation
         </Link>
         <ul className="flex items-center ml-5">
           <li className="flex items-center">
